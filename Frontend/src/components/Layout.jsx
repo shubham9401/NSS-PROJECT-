18e1142a4a46665ceeb76be5cd800266c2529b3d
Shubham Agrawal <gr8Shubham07@gmail.com>
2026-01-19 01:06:30 +0530

    Changed Layout Functionality

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -90,11 +90,7 @@ export default function Layout({ role }) {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
               </div>
-<<<<<<< Updated upstream
-              <span><center>HopeHands Foundation</center></span>
-=======
               <span>HopeHands Foundation</span>
->>>>>>> Stashed changes
             </div>
           </div>
         </div>
